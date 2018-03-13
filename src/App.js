cc365a5839ec075a411a3fe807e57eb6b8c0f012
Danny Ngo <danny.ngo9@gmail.com>
2018-03-13 16:08:13 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      comments: [],
-      link:""
-
+      comments: [],//array to save comments from the JSON object
+      link:""//track the next URL to load more comments
     }
     this.eachComment = this.eachComment.bind(this)
     this.loadMore = this.loadMore.bind(this)
@@ -22,11 +21,11 @@ class App extends Component {
   
       fetch('http://jsonplaceholder.typicode.com/comments?_page=1&_limit=20')
       .then(function (response){
-        self.setState({link:response.headers.get("link")})
-        return response
+        self.setState({link:response.headers.get("link")})//set the state of link. by getting the next link from the header response
+        return response //return the repsonse
       })
       .then(response => response.json())
-      .then(json => json.forEach( el => self.add(el)))
+      .then(json => json.forEach( el => self.add(el)))//loop through and call the add method on each node
       
       
     
@@ -36,9 +35,9 @@ class App extends Component {
   add(text){
     this.setState(prevState => ({
       comments: [
-        ...prevState.comments,
+        ...prevState.comments,//previous state
         {
-          postID: text.postId,
+          postID: text.postId,//add the new comments
           id: text.id,
           name: text.name,
           email: text.email,
@@ -49,29 +48,29 @@ class App extends Component {
     }))
   }
 
-  loadMore(){
+  loadMore(){//loads more comments
     var self = this
     var state = this.state.link;
-    var parse = require('parse-link-header');
+    var parse = require('parse-link-header');//using parse header package found here https://www.npmjs.com/package/parse-link-header
     var parsed = parse(state);
     console.log(parsed.next);
-    if(parsed.next){
+    if(parsed.next){//check if theres any next link ref
       
-      fetch(parsed.next.url)
+      fetch(parsed.next.url)//if there is fetch 
       .then(function (response){
-        self.setState({link:response.headers.get("link")})
+        self.setState({link:response.headers.get("link")})//set the new next link to state
         return response
       })
       .then(response => response.json())
-      .then(json => json.forEach( el => self.add(el)))
+      .then(json => json.forEach( el => self.add(el)))//add next set of comments to state
     }
-    else{
-      console.log('n')
+    else{//if there no next link do something
+      console.log('no next link')
     }
 
   }
 
-  eachComment(comment, i) {
+  eachComment(comment, i) {//comment template
     return (
         <Comment key={i} index={i}>
           <div className="indComment">
